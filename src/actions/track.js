c44a6ec8f2f203bb2ac8d5525357a3dc32d842a0
Todor Krastev <to6esko69@gmail.com>
2016-07-01 09:32:32 +0300

    add redux

---
+++ src/actions/track.js
@@ -0,0 +1,9 @@
+import * as actionTypes from '../constants/actionTypes';
+
+
+export function setTracks(tracks) {
+    return {
+        type: actionTypes.TRACKS_SET,
+        tracks
+    };
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
 import Stream from './components/Stream';
 
 const tracks = [
@@ -10,6 +12,8 @@ const tracks = [
         title: 'Some other track'
     }
 ];
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
 
-ReactDOM.render(<Stream tracks={tracks}/>,
+ReactDOM.render(<Stream />,
     document.getElementById('app'));
